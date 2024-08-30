02738bb62e993dfde1a837721fa663569092f85b
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-30 19:41:56 +0530

    1..3

--- src/components/User1.js
+++ src/components/User1.js
@@ -6,7 +6,7 @@ const User1 = () => {
         <h1>User Details</h1>
         <p><strong>Name: </strong>Leanne Graham</p>
         <p><strong>Username: </strong>Bret</p>
-        <p><strong>Email: </strong>amit@gamil.com</p>
+        <p><strong>Email: </strong>Sincere@april.biz</p>
         <p><strong>Phone: </strong>1234567890</p>
         <p><strong>Website: </strong>www.amit.com</p> 
     </div>
