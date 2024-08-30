2f20ca19d69688fd44c8a264cd32e2a0ed96be54
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-31 02:10:05 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,32 @@ import Navbar from "./Navbar";
 import Loading from "./Loading";
 import { Suspense, lazy } from "react";
 
-const User1 = lazy(() => import('./User1'));
-const User2 = lazy(() => import('./User2'));
-const User3 = lazy(() => import('./User3'));
-const User4 = lazy(() => import('./User4'));
-const User5 = lazy(() => import('./User5'));
-const User6 = lazy(() => import('./User6'));
-const User7 = lazy(() => import('./User7'));
-const User8 = lazy(() => import('./User8'));
-const User9 = lazy(() => import('./User9'));
-const User10 = lazy(() => import('./User10'));
+// const User1 = lazy(() => import('./User1'));
+// const User2 = lazy(() => import('./User2'));
+// const User3 = lazy(() => import('./User3'));
+// const User4 = lazy(() => import('./User4'));
+// const User5 = lazy(() => import('./User5'));
+// const User6 = lazy(() => import('./User6'));
+// const User7 = lazy(() => import('./User7'));
+// const User8 = lazy(() => import('./User8'));
+// const User9 = lazy(() => import('./User9'));
+// const User10 = lazy(() => import('./User10'));
+
+import User1 from './User1';
+import User2 from './User2';
+import User3 from './User3';
+import User4 from './User4';
+import User5 from './User5';
+import User6 from './User6';
+import User7 from './User7';
+import User8 from './User8';
+import User9 from './User9';
+import User10 from './User10';
 
 const App = () => {
+  
   return (
+    
     <BrowserRouter>
       
       <Routes>
