23582eca8056850a158b3ec63bcbfaad5f092e11
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-30 19:44:17 +0530

    updated

--- src/components/User1.js
+++ src/components/User1.js
@@ -7,7 +7,7 @@ const User1 = () => {
         <p><strong>Name: </strong>Leanne Graham</p>
         <p><strong>Username: </strong>Bret</p>
         <p><strong>Email: </strong>Sincere@april.biz</p>
-        <p><strong>Phone: </strong>1234567890</p>
+        <p><strong>Phone: </strong>1-770-736-8031 x56442</p>
         <p><strong>Website: </strong>www.amit.com</p> 
     </div>
   )
