2e6fb06bbc460ca2302e5e323d7a6237d73202a2
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-30 19:34:19 +0530

    1.2

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,38 @@
-
 import React from "react";
 import './../styles/App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
+import Loading from "./Loading";
+import { Suspense, lazy } from "react";
+
 import User1 from "./User1";
 import User2 from "./User2";
 import User3 from "./User3";
 import User4 from "./User4";
 import User5 from "./User5";
-import Loading from "./Loading";
+
+// Lazy loading the user components
+// const User1 = lazy(() => import('./User1'));
+// const User2 = lazy(() => import('./User2'));
+// const User3 = lazy(() => import('./User3'));
+// const User4 = lazy(() => import('./User4'));
+// const User5 = lazy(() => import('./User5'));
+
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navbar />} />
-      <Route path="/users/1" element={<User1 />? <User1 /> : <Loading />} />
-      <Route path="/users/2" element={<User2 />? <User1 /> : <Loading />} />
-      <Route path="/users/3" element={<User3 />? <User1 /> : <Loading />} />
-      <Route path="/users/4" element={<User4 />? <User1 /> : <Loading />} />
-      <Route path="/users/5" element={<User5 />? <User1 /> : <Loading />} />
-    </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/users/1" element={<User1 />} />
+          <Route path="/users/2" element={<User2 />} />
+          <Route path="/users/3" element={<User3 />} />
+          <Route path="/users/4" element={<User4 />} />
+          <Route path="/users/5" element={<User5 />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/User2.js
+++ src/components/User2.js
@@ -4,7 +4,7 @@ const User2 = () => {
   return (
     <div>
         <h1>User Details</h1>
-        <p><strong>Name: </strong>Vijay</p>
+        <p><strong>Name: </strong>Bret</p>
         <p><strong>Username: </strong>Vijay@</p>
         <p><strong>Email: </strong>vijay@gamil.com</p>
         <p><strong>Phone: </strong>2156626266</p>
