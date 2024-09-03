d563b26246097c6105888625ee43d332a2c6dd94
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-04 03:53:04 +0530

    1258

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080', // Replace with your base URL
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Ensure this pattern matches your test files
     // Add other configurations here (e.g., `viewportWidth`, `viewportHeight`, etc.)
   },
 });
