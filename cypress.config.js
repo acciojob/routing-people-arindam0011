3ca23873a607d91a67ac0d5d8980763e18587dd9
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-04 04:15:11 +0530

    456

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080', 
+    baseUrl: null, 
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
 
