8cd651bb229fbab58a3f017c3f3a7387a1bd142b
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-04 03:40:04 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -5,97 +5,20 @@ import Navbar from "./Navbar";
 import Loading from "./Loading";
 import { Suspense, lazy } from "react";
 
-// const User1 = lazy(() => import('./User1'));
-// const User2 = lazy(() => import('./User2'));
-// const User3 = lazy(() => import('./User3'));
-// const User4 = lazy(() => import('./User4'));
-// const User5 = lazy(() => import('./User5'));
-// const User6 = lazy(() => import('./User6'));
-// const User7 = lazy(() => import('./User7'));
-// const User8 = lazy(() => import('./User8'));
-// const User9 = lazy(() => import('./User9'));
-// const User10 = lazy(() => import('./User10'));
 
-import User1 from './User1';
-import User2 from './User2';
-import User3 from './User3';
-import User4 from './User4';
-import User5 from './User5';
-import User6 from './User6';
-import User7 from './User7';
-import User8 from './User8';
-import User9 from './User9';
-import User10 from './User10';
-
+import User from './User'
 
 const App = () => {
   return (
     <BrowserRouter>
-    
+    <Suspense fallback={<Loading />}>
         <Routes>
          <Route path="/" element={<Navbar />} />
 
-          <Route path="/users/1" element={
-           
-            <User1 />
-          
-            } />
-
-          <Route path="/users/2" element={
-           
-            <User2 />
-          
-            } />
-
-          <Route path="/users/3" element={
-           
-            <User3 />
-          
-            } />
-
-          <Route path="/users/4" element={
-           
-            <User4 />
-          
-            } />  
-
-          <Route path="/users/5" element={
-           
-            <User5 />
-          
-            } />
-
-          <Route path="/users/6" element={
-           
-            <User6 />
-          
-            } />
-
-          <Route path="/users/7" element={
-           
-            <User7 />
-          
-            } />
-
-          <Route path="/users/8" element={
-           
-            <User8 />
-          
-            } />
-
-          <Route path="/users/9" element={
-           
-            <User9 />
-          
-            } />
-
-          <Route path="/users/10" element={
-           
-            <User10 />
-          
-            } />
+         <Route path="/users/:id" element={<User />} />
           
         </Routes>
+    </Suspense>
     </BrowserRouter>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
-
+import 'regenerator-runtime/runtime'; 
 const Navbar = () => {
+  const[usersData, setUsersData] = useState([])
+  useEffect(() => {
+    const getData= async () =>{
+      try{
+        const responce = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await responce.json()
+        setUsersData(data)
+      }
+      catch(error){
+           console.log(error)
+      }
+    }
+    getData()
+  },[])
   return (
     <div>
       <h1>User List</h1>
       <ul>
-        <Link to="/users/1"><li>Leanne Graham</li></Link>
-        <Link to="/users/2"><li>Ervin Howell</li></Link>
-        <Link to="/users/3"><li>Clementine Bauch</li></Link>
-        <Link to="/users/4"><li>Patricia Lebsack</li></Link>
-        <Link to="/users/5"><li>Chelsey Dietrich</li></Link>
-        <Link to="/users/6"><li>Mrs. Dennis Schulist</li></Link>
-        <Link to="/users/7"><li>Kurtis Weissnat</li></Link>
-        <Link to="/users/8"><li>Nicholas Runolfsdottir V</li></Link>
-        <Link to="/users/9"><li>Glenna Reichert</li></Link>
-        <Link to="/users/10"><li>Clementina DuBuque</li></Link>
+        {
+          usersData.map(user => <li key={user.id}><Link to={`/users/${user.id}`}>{user.name}</Link></li>)
+        }
       </ul>
     </div>
   )
