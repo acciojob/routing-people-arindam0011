0c7dc6204cff6cb4c5248448dc6bd5e4dacf6912
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-04 04:01:33 +0530

    1245

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080', // Replace with your base URL
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Ensure this pattern matches your test files
+    baseUrl: "http://localhost:8080", // Replace with your base URL
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Ensure this pattern matches your test files
     // Add other configurations here (e.g., `viewportWidth`, `viewportHeight`, etc.)
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
   },
 });
